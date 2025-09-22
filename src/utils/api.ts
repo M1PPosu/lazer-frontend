d792ad3459ae739a93017ce7a698021225498e35
咕谷酱 <gu@gmoe.cc>
2025-09-23 03:33:55 +0800

    修改api url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,8 +2,8 @@ import axios, { type AxiosRequestConfig } from 'axios';
 import toast from 'react-hot-toast';
 
 // API base URL - adjust this to match your osu! API server
-//const API_BASE_URL = 'https://lazer-api.g0v0.top';
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://lazer-api.g0v0.top';
+//const API_BASE_URL = 'http://127.0.0.1:8000';
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
