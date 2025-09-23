5ef0c703c71dc0fd01819b5da882b9f894c02f34
咕谷酱 <gu@gmoe.cc>
2025-09-24 00:45:35 +0800

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import HeroSection from '../components/Home/HeroSection';
 import HomeFooter from '../components/Home/HomeFooter';
-import BeatmapUrlLoader from '../components/Beatmap/BeatmapUrlLoader';
 
 const HomePage: React.FC = () => (
   <div className="min-h-screen bg-slate-50 dark:bg-slate-900 flex flex-col">
